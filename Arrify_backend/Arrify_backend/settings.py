00321b64b9ddbc5b142d43a09b4341a0b4db1040
Prateek <prateeksagar806@gmail.com>
2023-12-04 09:00:09 +0530

    Creating the models to store the data of Visitors and Employees details

--- Arrify_backend/Arrify_backend/settings.py
+++ Arrify_backend/Arrify_backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     "corsheaders",
+    "phonenumber_field",
 ]
 
 MIDDLEWARE = [
--- Arrify_backend/api/models.py
+++ Arrify_backend/api/models.py
@@ -1,2 +1,21 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
+class EmployeeModel(models.Model):
+    email = models.EmailField(max_length=254)
+    name = models.CharField(max_length=254)
+    designation = models.CharField(max_length=50)
+    available = models.BooleanField(default=True)
+    password = models.CharField(max_length=100)
+
+
+class VisitorsModel(models.Model):
+    email = models.EmailField(max_length=254, null=False, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    purpose = models.CharField(max_length=50, null=False)
+    to_meet  = models.ForeignKey("EmployeeModel", on_delete=models.CASCADE, null=False)
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    adress = models.CharField(max_length=512)
+    check_in = models.DateTimeField(auto_now_add=True)
+    check_out = models.DateTimeField(null=True)
