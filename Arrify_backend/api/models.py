86f3a54ff27cd7d44dc3cf88df7b620a7f5ecfd6
Prateek <prateeksagar806@gmail.com>
2023-12-04 09:06:53 +0530

    Update the comments in the model.py

--- Arrify_backend/api/models.py
+++ Arrify_backend/api/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 class EmployeeModel(models.Model):
+    """
+    Use to store details of employees, consisting of their name, designation which is
+    helpful to create the dashboard of an employee and help to login in the their dashboard.
+    """
     email = models.EmailField(max_length=254)
     name = models.CharField(max_length=254)
     designation = models.CharField(max_length=50)
@@ -10,11 +14,15 @@ class EmployeeModel(models.Model):
 
 
 class VisitorsModel(models.Model):
-    email = models.EmailField(max_length=254, null=False, unique=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    """
+    This model stores all the information about visitors that helps the employee to 
+    track and achedule the visitors.
+    """
+    email = models.EmailField(max_length=254, null=False, unique=True)  
+    first_name = models.CharField(max_length=50)                       
+    last_name = models.CharField(max_length=50)                        
     purpose = models.CharField(max_length=50, null=False)
-    to_meet  = models.ForeignKey("EmployeeModel", on_delete=models.CASCADE, null=False)
+    to_meet  = models.ForeignKey("EmployeeModel", on_delete=models.CASCADE, null=False)     # Designation of the employee
     phone = PhoneNumberField(null=False, blank=False, unique=True)
     adress = models.CharField(max_length=512)
     check_in = models.DateTimeField(auto_now_add=True)
