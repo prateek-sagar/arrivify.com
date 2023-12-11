fddca388c0884743e83e0c7123f9afc697ec8263
Prateek <prateeksagar806@gmail.com>
2023-12-11 18:03:18 +0530

    Adding the tailwind css framework to work efficiently.

--- Arrivify_interface/src/App.jsx
+++ Arrivify_interface/src/App.jsx
@@ -1,8 +1,12 @@
-import './App.css'
-import Home from './pages/Home'
+// import './App.css'
+import Home from "./pages/Home";
 
 function App() {
-  <Home />
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
-export default App
+export default App;
