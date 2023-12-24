4e39b5b8787963e9b108097f2f831a7faddf831e
Prateek <prateeksagar806@gmail.com>
2023-12-24 21:12:37 +0530

    Update due to backend runserver

--- Arrivify_interface/src/App.jsx
+++ Arrivify_interface/src/App.jsx
@@ -1,42 +1,8 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import axios from 'axios'
+// import './App.css'
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-  let _title = "TITLE_FROM_FRONTEND"
-  let _body = "The body is written by maths."
-  async function getDetails() {
-    try {
-      let res = await axios.get("http://127.0.0.1:8000/api")
-      console.log(res)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  async function post_data(){
-    try {
-      let res = await axios.post("http://127.0.0.1:8000/api", {
-        title: _title,
-        body: _body
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      console.log(res)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  getDetails();
-  return (
-    <div>
-      <p></p>
-      <button onClick={post_data}>Post</button>
-    </div>
-  )
+  return <Home />;
 }
 
-export default App
+export default App;
---
+++ Arrivify_interface/src/pages/components/CheckInModal.jsx
@@ -0,0 +1,136 @@
+import React, { useEffect } from "react";
+import { IoClose } from "react-icons/io5";
+const to_meet = async () => {
+  try {
+    let res = await fetch("http://127.0.0.1:8000/api/employees");
+    if (res.ok) {
+      console.log(data);
+      let data = await res.json();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  console.log("to meet data");
+};
+
+export default function CheckInModal(props) {
+  useEffect(() => {
+    to_meet();
+  }, []);
+
+  const handleClose = () => {
+    props.onClose();
+  };
+  const close_style = {
+    color: "white",
+    fontSize: "2em",
+    backgroundColor: "black",
+    borderRadius: "100%",
+  };
+  if (!props.visible) return null;
+
+  return (
+    <>
+      <div
+        className="relative z-10"
+        aria-labelledby="modal-title"
+        role="dialog"
+        aria-modal="true"
+      >
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className="relative transform rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <div className="relative px-4 pb-4 pt-5 sm:p-6 sm:pb-4 rounded-lg">
+                <div className="flex justify-center items-center">
+                  <form className="w-full max-w-xs">
+                    <div className="mb-4">
+                      <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="username"
+                      >
+                        First Name
+                      </label>
+                      <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="username"
+                        type="text"
+                        placeholder="Username"
+                      />
+                    </div>
+                    <div className="mb-6">
+                      <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="password"
+                      >
+                        Email
+                      </label>
+                      <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        type="email"
+                        placeholder="abc@mail.com"
+                      />
+                    </div>
+                    {/* Address */}
+                    <div className="mb-6">
+                      <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="password"
+                      >
+                        Address
+                      </label>
+                      <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        type="email"
+                        placeholder="abc@mail.com"
+                      />
+                    </div>
+                    {/* purpose */}
+                    <div className="mb-6">
+                      <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="password"
+                      >
+                        Purpose
+                      </label>
+                      <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        type="email"
+                        placeholder="abc@mail.com"
+                      />
+                    </div>
+                    {/* to meet */}
+                    <div className="mb-6">
+                      <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="password"
+                      >
+                        To Meet
+                      </label>
+                      <input
+                        className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        type="email"
+                        placeholder="abc@mail.com"
+                      />
+                    </div>
+                    <div className="flex items-center justify-end">
+                      <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="button"
+                      >
+                        Check In
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <div className="absolute -top-3 -right-3 cursor-pointer rounded-full flex items-center justify-center">
+                  <IoClose style={close_style} onClick={handleClose} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
