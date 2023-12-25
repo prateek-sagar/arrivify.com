486ad151510893de1f98cb026b9570e20c1e1045
Prateek <prateeksagar806@gmail.com>
2023-12-25 16:46:01 +0530

    Make the home page more responsive and recreate it

--- Arrivify_interface/src/pages/Home.jsx
+++ Arrivify_interface/src/pages/Home.jsx
@@ -10,44 +10,37 @@ export default function Home() {
   return (
     // whole screen
     <>
-      <div className=" h-screen">
-        {/* List of Login and Check in  */}
-        <ul className="w-full h-full md:flex flex-row justify-evenly items-center ">
-          <li className="md:w-1/2 md:h-full h-1/2 flex flex-col items-center justify-center">
-            {/* Check in card */}
-            <div className="rounded-md w-2/3 h-4/5 flex flex-col items-center justify-center bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl shadow-xl group-hover:shadow-2xl shadow-gray-400">
-              <h1 className="font-Yantramanav font-black text-6xl my-20">
-                For Visitors
-              </h1>
-              <p className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                <button
-                  onClick={() => {
-                    setShowCheckIn(true);
-                  }}
-                  className="relative px-5 py-2.5 font-Yantramanav font-black text-xl uppercase transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 cursor-pointer"
-                >
-                  Check In
-                </button>
-              </p>
-            </div>
+      <div className="w-full h-screen bg-white flex-col justify-center items-center inline-flex">
+        <ul className="self-stretch grow shrink basis-0 px-4 py-8 flex-col justify-center items-center gap-4 inline-flex md:flex-row md:gap-8 md:py-12 md:px-8 lg:p-12 lg:gap-16">
+          <li className="grow shrink basis-0 self-stretch bg-zinc-300 flex flex-col justify-center items-center rounded-xl hover:scale-105 duration-300 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl shadow-xl group-hover:shadow-2xl shadow-gray-400">
+            <h1 className="w-full px-4 md:px-8 font-Yantramanav font-black text-6xl md:text-7xl lg:text-8xl py-4 md:py-8">
+              For <br /> Visitors
+            </h1>
+            <p className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+              <button
+                onClick={() => {
+                  setShowCheckIn(true);
+                }}
+                className="relative px-5 py-2.5 font-Yantramanav font-black text-xl uppercase transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 cursor-pointer"
+              >
+                Check In
+              </button>
+            </p>
           </li>
-          <li className="md:w-1/2 md:h-full h-1/2 flex flex-col items-center justify-center">
-            {/* Login Card */}
-            <div className="rounded-md w-2/3 h-4/5 flex flex-col items-center justify-center bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl shadow-xl group-hover:shadow-2xl shadow-gray-400 overflow-hidden">
-              <h1 className="font-Yantramanav left-20 font-black text-6xl my-20">
-                For Employees
-              </h1>
-              <p className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                <button
-                  onClick={() => {
-                    setShowLogin(true);
-                  }}
-                  className="px-5 py-2.5 uppercase transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 cursor-pointer"
-                >
-                  Login
-                </button>
-              </p>
-            </div>
+          <li className="grow shrink basis-0 self-stretch bg-zinc-300 flex flex-col justify-center items-center rounded-xl hover:scale-105 duration-300 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl shadow-xl group-hover:shadow-2xl shadow-gray-400">
+            <h1 className="w-full px-4 md:px-8 font-Yantramanav font-black text-6xl md:text-7xl lg:text-8xl py-4 md:py-8">
+              For <br /> Employees
+            </h1>
+            <p className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+              <button
+                onClick={() => {
+                  setShowLogin(true);
+                }}
+                className="relative px-5 py-2.5 font-Yantramanav font-black text-xl uppercase transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 cursor-pointer"
+              >
+                Login
+              </button>
+            </p>
           </li>
         </ul>
       </div>
