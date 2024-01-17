fd41aaef623ea7a95e917b45ae72c04c13fed48d
Prateek <prateeksagar806@gmail.com>
2024-01-17 10:26:32 +0530

    Clearing all the unecessary components and make them in a link

--- Arrivify_interface/src/pages/Home.jsx
+++ Arrivify_interface/src/pages/Home.jsx
@@ -1,68 +1,42 @@
-import React, { useState } from "react";
-import Login_modal from "./components/Login_modal";
-import CheckInModal from "./components/CheckInModal";
-import VisitorsCard from "./components/VisitorsCard";
+import React from "react";
 import NavBar from "./components/NavBar";
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  const [showLogin, setShowLogin] = useState(false);
-  const [showCheckIn, setShowCheckIn] = useState(false);
-  const [showVisitorsCard, setShowVisitorsCard] = useState(false);
-  const handleCloseLogin = () => setShowLogin(false);
-  const handleCloseCheckIn = () => setShowCheckIn(false);
-  const handleCloseVisitorsCard = () => setShowVisitorsCard(false);
-  const handleCheckInWindow = () => {
-    setShowCheckIn(false);
-    setShowVisitorsCard(true);
-  };
   return (
     // whole screen
     <>
-      <div className="w-full h-screen bg-white flex-col justify-center items-center inline-flex">
-        <NavBar />
-        <ul className="self-stretch grow shrink basis-0 px-4 py-8 flex-col justify-center items-center gap-4 inline-flex md:flex-row md:gap-8 md:py-12 md:px-8 lg:p-12 lg:gap-16 mt-4">
-          <li className="grow shrink basis-0 self-stretch bg-zinc-300 flex flex-col justify-center items-center rounded-xl hover:scale-105 duration-300 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl shadow-xl group-hover:shadow-2xl shadow-gray-400">
-            <h1 className="w-full px-4 md:px-8 font-Yantramanav font-black text-6xl md:text-7xl lg:text-8xl py-4 md:py-8">
-              For <br /> Visitors
-            </h1>
-            <p className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-              <button
-                onClick={() => {
-                  setShowCheckIn(true);
-                }}
-                className="relative px-5 py-2.5 font-Yantramanav font-black text-xl uppercase transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 cursor-pointer"
+      <NavBar />
+      <div className="w-full bg-white flex-col justify-center items-center inline-flex">
+        <div className="w-full grid grid-cols-1 h-[100dvh]">
+          <ul className="self-stretch grow shrink basis-0 px-4 py-8 flex-col justify-center items-center gap-4 inline-flex md:flex-row md:gap-8 md:py-12 md:px-8 lg:p-12 lg:gap-16 mt-4">
+            <li className="grow shrink basis-0 self-stretch flex flex-col justify-center items-center rounded-xl duration-300 bg-red-300">
+              <h1 className="w-full px-4 md:px-8 font-Yantramanav font-black text-6xl md:text-7xl lg:text-8xl py-4 md:py-8">
+                For <br /> Visitors
+              </h1>
+              <Link
+                to={"/checkin"}
+                class="bg-blue-500 hover:bg-blue-700 text-xl text-white font-medium py-2 px-4 rounded-full"
               >
-                Check In
-              </button>
-            </p>
-          </li>
-          <li className="grow shrink basis-0 self-stretch bg-zinc-300 flex flex-col justify-center items-center rounded-xl hover:scale-105 duration-300 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl shadow-xl group-hover:shadow-2xl shadow-gray-400">
-            <h1 className="w-full px-4 md:px-8 font-Yantramanav font-black text-6xl md:text-7xl lg:text-8xl py-4 md:py-8">
-              For <br /> Employees
-            </h1>
-            <p className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-              <button
-                onClick={() => {
-                  setShowLogin(true);
-                }}
-                className="relative px-5 py-2.5 font-Yantramanav font-black text-xl uppercase transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 cursor-pointer"
+                CheckIn
+              </Link>
+            </li>
+            <li className="grow shrink basis-0 self-stretch flex flex-col justify-center items-center rounded-xl duration-300 bg-yellow-200">
+              <h1 className="w-full px-4 md:px-8 font-Yantramanav font-black text-6xl md:text-7xl lg:text-8xl py-4 md:py-8">
+                For <br /> Employees
+              </h1>
+              <Link
+                to={"/login"}
+                class="bg-blue-500 hover:bg-blue-700 text-xl text-white font-medium py-2 px-4 rounded-full"
               >
                 Login
-              </button>
-            </p>
-          </li>
-        </ul>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        {/* to be add in future */}
+        {/* <Footer /> */}
       </div>
-      {/* Modal Code */}
-      <Login_modal onClose={handleCloseLogin} visible={showLogin} />
-      <CheckInModal
-        onClose={handleCloseCheckIn}
-        checkin={handleCheckInWindow}
-        visible={showCheckIn}
-      />
-      <VisitorsCard
-        onclose={handleCloseVisitorsCard}
-        visible={showVisitorsCard}
-      />
     </>
   );
 }
