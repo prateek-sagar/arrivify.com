ba983bed405ca1ebc37f7d95c27044a59646a007
Prateek <prateeksagar806@gmail.com>
2024-01-17 09:53:56 +0530

    The code looks copying of data so delete the App.jsx and control all the things from main.jsx file

--- Arrivify_interface/src/main.jsx
+++ Arrivify_interface/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />;
+  </React.StrictMode>
+);
