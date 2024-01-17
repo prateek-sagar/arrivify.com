9d4a7554c1bf8a1ba1bd69eaf9f3a4f41720de53
Prateek <prateeksagar806@gmail.com>
2024-01-17 10:27:55 +0530

    Adding the link of the login and checkin page

--- Arrivify_interface/src/pages/components/NavBar.jsx
+++ Arrivify_interface/src/pages/components/NavBar.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <div className="w-full bg-black h-12 flex justify-center items-center fixed top-0">
       <div className="w-1/2  text-white text-2xl px-[10%]">Arrivify</div>
       <div className="w-1/2 grid grid-flow-col justify-end items-center gap-4 px-[10%] h-full overflow-hidden">
-        <button className="outline outline-2 hover:bg-white hover:text-black w-fit px-2 h-8 text-white text-base rounded-full">
+        <Link
+          to={"/checkin"}
+          className="outline outline-2 hover:bg-white hover:text-black w-fit px-2 h-8 text-white text-base rounded-full flex items-center justify-center"
+        >
           CheckIn
-        </button>
-        <button className="outline outline-2 hover:bg-white hover:text-black w-fit px-2 h-8 text-white text-base rounded-full">
+        </Link>
+        <Link
+          to={"/login"}
+          className="outline outline-2 hover:bg-white hover:text-black w-fit px-2 h-8 text-white text-base rounded-full flex items-center justify-center"
+        >
           Login
-        </button>
+        </Link>
       </div>
     </div>
   );
