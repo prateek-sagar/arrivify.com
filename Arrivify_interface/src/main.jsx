cf93f7edcd0584ba1dfea0bf270bb3c55b478628
Prateek <prateeksagar806@gmail.com>
2024-01-17 10:30:33 +0530

    Adding the path in the router

--- Arrivify_interface/src/main.jsx
+++ Arrivify_interface/src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./pages/Home";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login_modal from "./pages/components/Login_modal";
+import CheckInModal from "./pages/components/CheckInModal";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+  {
+    path: "/login",
+    element: <Login_modal />,
+  },
+  { path: "/checkin", element: <CheckInModal /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
