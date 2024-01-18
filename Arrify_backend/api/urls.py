02b4594bfc55bb6d4a08929d5dc4e7dc214caacc
Prateek <prateeksagar806@gmail.com>
2024-01-18 20:46:52 +0530

    Adding Logout Functionality to the website

--- Arrify_backend/api/urls.py
+++ Arrify_backend/api/urls.py
@@ -3,5 +3,6 @@ from .views import *
 urlpatterns = [
     path("visitors/", VisitorsView.as_view(), name=""),
     path("employees/", EmployeesView.as_view(), name=""),
-    path("home/", HomeView.as_view(), name="home")
+    path("home/", HomeView.as_view(), name="home"),
+    path('logout/', LogoutView.as_view(), name ='logout'),
 ]
--- Arrify_backend/api/views.py
+++ Arrify_backend/api/views.py
@@ -44,3 +44,13 @@ class HomeView(APIView):
         content = {'message': 'Welcome to the JWT Authentication page using React Js and Django!'}
         return Response(content)
     
+class LogoutView(APIView):
+     permission_classes = (IsAuthenticated,)
+     def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
